a955edeec11ad953ae049c12c0e252f5f94975e9
Raphaël Duhen <raphael.duhen@ign.fr>
2024-04-30 17:08:56 +0200

    docs: add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# Node graph
+
+Simple node graph implementation.
+
+## How to run
+
+```bash
+# First time only
+npm install
+
+npx tsc --init
+node out/index.js
+```
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as graph from "./graph";
 const g = new graph.Graph();
 console.dir(g, { depth: null });
 
+// Keeping outside references to relevant nodes makes the code more readable.
+// Could also make Graph::set return the node.
 const five = new graph.InputNode(5, graph.BuiltinType.Number);
 const succ = new graph.ProcessorNode(
   new Map(Object.entries({ value: five })),
@@ -15,8 +17,8 @@ g.set("succ", succ);
 
 g.validate();
 
-// Getting the output like this directly from a node (instead of a designated
-// graph output) makes implementing a "preview" mode trivial.
+// Getting the output like this directly from a node (instead of e.g. a
+// designated graph output) makes implementing a "preview" mode trivial.
 const output = succ.getOutput(0);
 
 console.dir(g, { depth: null });
